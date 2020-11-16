6ad8c90baa7625de3b0955b9fd738bcc37345138
Ben Kemp <benjokemp@gmail.com>
2020-11-16 14:29:30 +0100

    first commit

---
+++ src/components/Currency.vue
@@ -0,0 +1,39 @@
+<template>
+<div class="container">
+    <div>{{code}}</div>
+    <div v-for="(value, key) in currency" :key="key" class="currency_details">
+        <div class="currency_key">{{key}}</div>
+        <div class="currency_values">{{value}}</div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Currency',
+  props: {
+    currency: Object,
+    code: String
+  },
+  data: () => {
+    return {}
+  },
+}
+</script>
+<style lang="scss" scoped>
+
+.container {
+  margin: 10px;
+  display: flex;
+  flex-direction: row;
+  border: solid black 1px;
+  border-radius: 5px;
+  justify-content: space-around;
+  .currency_details {
+    display:flex;
+    flex-direction: column;
+    border: 1px solid gray;
+    font-size: 12px;
+  }
+}
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,146 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class=container>
+    <div class="flex-grid">
+        <div v-for="(rate, index) in rates" :key="rate.last" class="col" >
+          <currency :currency="rate" :code="index"></currency>
+      </div>
+    </div>
+      <div class="form_container">
+        <form
+          @submit="checkForm"
+          method="post"
+        >
+          <p v-if="errors.length">
+            <b>Please correct the following error(s):</b>
+            <ul>
+              <li v-for="error in errors" :key="error">{{ error }}</li>
+            </ul>
+          </p>
+          <p>
+            <label for="amount">Amount</label>
+            <input
+              id="amount"
+              v-model="amount"
+              type="number"
+              name="amount"
+              min="0"
+            >
+          </p>
+          <div class="form_element">
+            <label for="currency">Base Currency</label>
+            <v-select id="currency" :options="keys" placeholder="base currency" @input="setSelected"/>
+          </div>
+          <p>
+            <input
+              type="submit"
+              value="Submit"
+            >
+          </p>
+        </form>
+    </div>
   </div>
 </template>
-
 <script>
+import axios from 'axios'
+import vSelect from 'vue-select'
+import Currency from './Currency'
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  components: {
+    vSelect,
+    Currency
+  },
+  data: () => {
+    return {
+      rates: {},
+      selected: null,
+      errors: [],
+      amount: null
+    }
+  },
+  computed:{
+    keys () {
+      return Object.keys(this.rates)
+    }
+  },
+  methods: {
+    async load () {
+      try {
+        const response = await axios.get('https://blockchain.info/ticker')
+        this.rates = response.data
+      } catch (e) {
+        console.error(e)
+      }
+    },
+    checkForm(e) {
+      e.preventDefault();
+      this.errors = [];
+      if (!this.amount) {
+        this.errors.push('please enter amount to be converted');
+      } else if (this.amount==0) {
+        this.errors.push('please enter an amount greater than zero');
+      } else {
+        fetch(`https://blockchain.info/tobtc?currency=${this.selected}&value=${this.amount}`)
+        .then(async res => {
+          if (res.status === 200) {
+             let conversion = await res.json();
+            alert(`${this.selected} ${this.amount} is worth ${conversion} bitcoins`);
+          } else if (res.status === 400) {
+            let errorResponse = await res.json();
+            this.errors.push(errorResponse.error);
+          }
+        })
+      }
+    },
+    setSelected(value){
+      this.selected = value
+    }
+  },
+  mounted () {
+    this.load();
+    setInterval(function () {
+      this.load();
+    }.bind(this), 100000); 
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style lang="scss">
+@import "vue-select/src/scss/vue-select.scss";
+  .container {
+    display: flex;
+    flex-direction: column;
+  }
+  .flex-grid {
+    display: flex;
+    flex-wrap: wrap;
+  }
+  .col {
+    flex-basis: 33.3%;
+  }
+
+  @media (max-width: 1200px) {
+    .col {
+      flex: 50%;
+    }
+  }
+  @media (max-width: 800px) {
+    .col {
+      flex: 100%;
+    }
+    .form_container {
+      width:100%!important
+    }
+  }
+  .form_container {
+    width: 50%;
+    border: black solid;
+    padding: 10px;
+    align-self: center;
+    .form_element {
+      display: flex;
+      flex-direction: column;
+      padding: 10px;
+    }
+  }
 </style>
